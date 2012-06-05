09d2e078385a9a693da7b5ff714c8cd75bef676c
mattn <mattn.jp@gmail.com>
2012-06-05 16:30:18 +0900

    first import.

---
+++ server.js
@@ -0,0 +1,5 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+  res.end('Hello Node 忍者\n');
+}).listen(80);
